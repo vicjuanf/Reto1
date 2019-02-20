63de4221a087e818c57d2613388aab9fc28a8388
vicjuanf <vicjuanf@gmail.com>
2019-02-20 12:19:43 +0100

    Add files via upload
    Versión inicial de mi programa

---
+++ miprograma.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int dato;
+
+	cout << "Introduce un dato: ";
+
+	cin >> dato;
+	dato = dato * 10;
+
+	printf("Dato = %i", dato);
+
+
+}
